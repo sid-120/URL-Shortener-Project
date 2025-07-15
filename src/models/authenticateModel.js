32b0fda2d23939ff7311d888732232c9f0d2fe2f
sid-120 <siddharthuzumaki2000@gmail.com>
2025-07-15 09:48:02 +0530

    Authentication models file

---
+++ src/models/authenticateModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const authSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+  },
+});
+
+module.exports = mongoose.model("Auth", authSchema);
