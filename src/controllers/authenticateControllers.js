90e343ae8a6fd5050d0bbb0e02f316cdd04694c2
sid-120 <siddharthuzumaki2000@gmail.com>
2025-07-19 12:18:14 +0530

    file updated.

--- src/controllers/authenticateControllers.js
+++ src/controllers/authenticateControllers.js
@@ -1,6 +1,12 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const Auth = require("../models/authenticateModel.js");
+const { Auth, LoginEvent } = require("../models/authenticateModel.js");
+
+// Login user functions
+const loginUser = async (req, res) => {
+  const logins = await LoginEvent.find().sort({ loggedInAt: -1 });
+  res.json(logins);
+};
 
 // Signup function
 const signup = async (req, res) => {
@@ -24,25 +30,44 @@ const signup = async (req, res) => {
 
 // SignIn function
 const signin = async (req, res) => {
-  const { email, password } = req.body;
-  // finding the user is present or not from the database
-  const user = await Auth.findOne({ email });
-  //    error if user not exist
-  if (!user) {
-    return res.status(400).json({ message: "User does not exist" });
-  }
-  // checking for the correctness of the password
-  const isPasswordCorrect = await bcrypt.compare(password, user.password);
-  if (!isPasswordCorrect) {
-    return res.status(400).json({ message: "Invalid password" });
-  }
+  try {
+    const { email, password } = req.body;
+    // finding the user is present or not from the database
+    const user = await Auth.findOne({ email });
+    //    error if user not exist
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+    // checking for the correctness of the password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
 
-  // generating a token for the user and my secret key here is 'secret'
-  const token = jwt.sign({ email: user.email, userId: user._id }, "secret", {
-    expiresIn: "1h",
-  });
-  res.status(200).json({ token, userId: user._id, email: user.email });
+    // console.log("User signed in:", user.email);
+
+    // generating a token for the user and my secret key here is 'secret'
+    const token = jwt.sign({ email: user.email, userId: user._id }, "secret", {
+      expiresIn: "1h",
+    });
+    // res.status(200).json({
+    //   message: "Signed in successfully",
+    //   token,
+    //   userId: user._id,
+    //   email: user.email,
+    // });
+
+    // save login event
+
+    await LoginEvent.create({ email: user.email });
+    res.status(200).json({
+      message: "Successfully signed in users list",
+      user: { token, id: user._id, email: user.email },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // exporting both functions signup() and signin()
-module.exports = { signup, signin };
+module.exports = { signup, signin, loginUser };
