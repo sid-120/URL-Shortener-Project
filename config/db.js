50821ae9c862256b201f0d39010979240d3a308f
sid-120 <siddharthuzumaki2000@gmail.com>
2025-07-19 12:17:14 +0530

    database connection updated

--- config/db.js
+++ config/db.js
@@ -3,12 +3,16 @@ const mongoose = require("mongoose");
 const url =
   "mongodb+srv://siddharth:12345@cluster-1.lvvs0tz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1";
 
-const connectDB = mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Atlas connected"))
-  .catch((err) => console.error("Connection error:", err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Atlas connected");
+  } catch (err) {
+    console.error("Connection error:", err);
+  }
+};
 
 module.exports = connectDB;
